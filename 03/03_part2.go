eed6993cc66e56f2288a63084b89be9bbf0f5a01
melops-hh <melops@protonmail.com>
2024-12-03 12:37:41 +0100

    day 3 part 2 wip

--- 03/03_part2.go
+++ 03/03_part2.go
@@ -9,6 +9,11 @@ import (
 	"strings"
 )
 
+// Wrong answers:
+// 78965138 to high
+// 146499494
+// 153469856 to high
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	sum := 0
@@ -23,32 +28,29 @@ func handleText(s string) int {
 	r, _ := regexp.Compile("mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don\\'t\\(\\)")
 	match := r.FindAllString(s, -1)
 	sum := 0
-	lastInstruction := 0
+	enabled := true
 	for _, mul := range match {
-		i, s := multiply(mul, lastInstruction)
-		fmt.Printf("multiply %s, lastInstruction: %d, sum: %d, newSum: %d\n", mul, lastInstruction, sum, s)
-		lastInstruction = i
-		if i == 0 || i == 1 {
+		newDo, s := multiply(mul, enabled)
+		fmt.Printf("multiply %s, do: %t, sum: %d, newDo: %t newSum: %d\n", mul, enabled, sum, newDo, s)
+		if enabled {
 			sum += s
 		}
+		enabled = newDo
 	}
 	return sum
 }
 
-func multiply(s string, lastI int) (int, int) {
-	instruction := strings.Split(s, "(")[0]
-	if instruction == "do" {
-		return 1, 0
-	} else if instruction == "don't" {
-		return 2, 0
-	} else if instruction == "mul" {
-		s := s[4 : len(s)-1]
-		split := strings.Split(s, ",")
-		a, _ := strconv.Atoi(split[0])
-		b, _ := strconv.Atoi(split[1])
-		return lastI, a * b
-	} else {
-		panic("Invalid instruction")
+func multiply(s string, enabled bool) (bool, int) {
+	if s == "do()" {
+		return true, 0
+	} else if s == "don't()" || !enabled {
+		return false, 0
 	}
+
+	s = s[4 : len(s)-1]
+	split := strings.Split(s, ",")
+	a, _ := strconv.Atoi(split[0])
+	b, _ := strconv.Atoi(split[1])
+	return true, a * b
 
 }
